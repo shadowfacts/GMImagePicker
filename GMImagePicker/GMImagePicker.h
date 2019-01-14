f91df47683bff98aed0a49b75633f007149271a3
Shadowfacts <me@shadowfacts.net>
2019-01-14 18:16:34 -0500

    Add xcodeproj

---
+++ GMImagePicker/GMImagePicker.h
@@ -0,0 +1,24 @@
+//
+//  GMImagePicker.h
+//  GMImagePicker
+//
+//  Created by Shadowfacts on 1/14/19.
+//  Copyright © 2019 Shadowfacts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GMImagePicker.
+FOUNDATION_EXPORT double GMImagePickerVersionNumber;
+
+//! Project version string for GMImagePicker.
+FOUNDATION_EXPORT const unsigned char GMImagePickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GMImagePicker/PublicHeader.h>
+
+#import <GMImagePicker/GMAlbumsViewCell.h>
+#import <GMImagePicker/GMAlbumsViewController.h>
+#import <GMImagePicker/GMGridViewCell.h>
+#import <GMImagePicker/GMGridViewController.h>
+#import <GMImagePicker/GMImagePickerController.h>
+
